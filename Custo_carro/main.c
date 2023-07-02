b912460453beb8a1a716731df003602989a2494b
Thor <102772895+thorcamara@users.noreply.github.com>
2023-07-02 12:27:14 -0300

    Initial commit.

---
+++ Custo_carro/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float calcLucroDist(float p_fab, float por_dist){
+	return p_fab * por_dist / 100;
+}
+
+float calcImpostos(float p_fab, float por_imp){
+	return p_fab * por_imp / 100;
+}
+
+float calcPrecoFinal(float p_fab, float v_dist, float v_imp){
+	return p_fab + v_dist + v_imp;
+}
+
+int main(int argc, char *argv[]) {
+	
+	float preco_f, porce_dist, porc_i, lucro_dist, imp, vlr_f;
+	
+	printf("Insira o preco de fabrica:\n");
+	scanf("%f", &preco_f);
+	printf("Insira o percentual de lucro do distribuidor:\n");
+	scanf("%f", &porce_dist);
+	printf("Insira o percentual de imposto:\n");
+	scanf("%f", &porc_i);
+	
+	lucro_dist = calcLucroDist(preco_f, porce_dist);
+	imp = calcImpostos(preco_f, porc_i);
+	vlr_f = calcPrecoFinal(preco_f, lucro_dist, imp);
+	
+	printf("O lucro do distribuidor: R$ %.2f.\n", lucro_dist);
+	printf("Impostos: R$ %.2f.\n", imp);
+	printf("Valor fina: R$ %.2f.\n", vlr_f);
+	
+	return 0;
+}
